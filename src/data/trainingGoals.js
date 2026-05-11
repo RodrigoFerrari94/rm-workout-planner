5f5b0c58662a5f97b16eb62d832a0ff6fdf98c02
Rodrigo Ferrari <rodrigoferrari94rf@gmail.com>
2026-05-11 06:17:04 -0300

    feat: add load percentage ranges to training goals

--- src/data/trainingGoals.js
+++ src/data/trainingGoals.js
@@ -3,17 +3,23 @@ export const trainingGoals = {
     sets: "4-6",
     reps: "1-5",
     rest: "180-300s",
+    maxPercentage: 0.95,
+    minPercentage: 0.85,
   },
 
   hypertrophy: {
     sets: "3-4",
     reps: "8-12",
     rest: "90-180s",
+    maxPercentage: 0.84,
+    minPercentage: 0.7,
   },
 
   endurance: {
     sets: "2-3",
     reps: "12-20",
     rest: "30-60s",
+    maxPercentage: 0.69,
+    minPercentage: 0.55,
   },
 };
