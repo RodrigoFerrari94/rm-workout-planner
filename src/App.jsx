620c58612464230ea0d07ea4f2146ce168908d30
Rodrigo Ferrari <rodrigoferrari94rf@gmail.com>
2026-05-25 05:20:55 -0300

    feat: add workout history page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import Calculator from "./pages/Calculator";
 import WorkoutBuilder from "./pages/WorkoutBuilder";
 import WorkoutSession from "./pages/WorkoutSession";
+import WorkoutHistory from "./pages/workoutHistory";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/calculator" element={<Calculator />} />
         <Route path="/workout-builder" element={<WorkoutBuilder />} />
         <Route path="/workout-session" element={<WorkoutSession />} />
+        <Route path="/workout-history" element={<WorkoutHistory />} />
       </Routes>
     </Router>
   );
