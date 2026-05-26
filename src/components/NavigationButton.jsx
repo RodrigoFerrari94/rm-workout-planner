a98e15ce9e84a55066b0522707194f46eeeda0e7
Rodrigo Ferrari <rodrigoferrari94rf@gmail.com>
2026-05-26 19:29:36 -0300

    refactor: allow props in navigation button

--- src/components/NavigationButton.jsx
+++ src/components/NavigationButton.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
-export default function NavigationButton({ children, to }) {
+export default function NavigationButton({ children, to, ...props }) {
   const navigate = useNavigate();
 
   return <Button onClick={() => navigate(to)}>{children}</Button>;
