573bea1638b7be1d5aa3c590ecbdd4a55f8a0ff3
Rodrigo Ferrari <rodrigoferrari94rf@gmail.com>
2026-05-30 17:09:19 -0300

    refactor: improve navbar layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,30 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link to="/">Home</Link> |<Link to="/login">Login</Link> |
-      <Link to="/calculator">Calculator</Link> |
-      <Link to="/workout-history">Workout History</Link> |
-    </nav>
+    <header className="navbar">
+      <Link to="/" className="navbar__brand">
+        <span className="navbar__brand-mark">RM</span>
+        <span className="navbar__brand-text">Workout Planner</span>
+      </Link>
+
+      <nav className="navbar__links" aria-label="Main navigation">
+        <Link className="navbar__link" to="/">
+          Home
+        </Link>
+
+        <Link className="navbar__link" to="/login">
+          Login
+        </Link>
+
+        <Link className="navbar__link" to="/calculator">
+          Calculator
+        </Link>
+
+        <Link className="navbar__link" to="/workout-history">
+          History
+        </Link>
+      </nav>
+    </header>
   );
 }
+
